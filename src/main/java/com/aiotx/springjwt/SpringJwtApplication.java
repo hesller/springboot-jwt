cd0f90bb95b57235c5ff6cad2baa6c9ed1cfa770
Hesller Huller <eng.hesller@gmail.com>
2022-06-20 23:42:27 -0300

    Finished implementation of security package

--- src/main/java/com/aiotx/springjwt/SpringJwtApplication.java
+++ src/main/java/com/aiotx/springjwt/SpringJwtApplication.java
@@ -7,12 +7,19 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class SpringJwtApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringJwtApplication.class, args);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
@@ -33,5 +40,7 @@ public class SpringJwtApplication {
             userService.addRoleToUser("mark", "ROLE_USER");
         };
     }
+
+
 
 }
--- src/main/java/com/aiotx/springjwt/services/ServiceImplementation.java
+++ src/main/java/com/aiotx/springjwt/services/ServiceImplementation.java
@@ -6,14 +6,36 @@ import com.aiotx.springjwt.repository.RoleRepository;
 import com.aiotx.springjwt.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @org.springframework.stereotype.Service @RequiredArgsConstructor @Transactional @Slf4j
-public class ServiceImplementation implements Service {
+public class ServiceImplementation implements Service, UserDetailsService {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            log.error("User not found in the database");
+            throw new UsernameNotFoundException("User not found in the database");
+        } else {
+            log.info("User found in the database: {}", username);
+        }
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        user.getRoles().forEach(role -> {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        });
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), authorities);
+    }
 
     @Override
     public User saveUser(User user) {
